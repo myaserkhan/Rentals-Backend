90f5f95dbf461ab848dac16ce60f475c14467664
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-03-11 02:26:14 +0500

    Adding seed file to verify that the data is working as expected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reservations, dependent: :destroy
 
-    validates :name, presence: true
+    validates :username, presence: true
     validates :email, uniqueness: true
 end
