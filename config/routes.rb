92ced250fd46e5d893cdc54b90a8de2513ce809e
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-03-11 01:34:02 +0500

    Creating migration files according to the ERD diagram decided

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :reservations
+  resources :cars
+  resources :users
   namespace :api do
     namespace :v1 do
 
---
+++ db/migrate/20230310203019_create_cars.rb
@@ -0,0 +1,14 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price
+      t.string :duration
+      t.string :imgurl
+      t.string :brand
+
+      t.timestamps
+    end
+  end
+end
