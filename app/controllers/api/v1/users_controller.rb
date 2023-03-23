4c972fe753cb928edfae591649672cffe10cafd5
Yasir Khan <khanyaser007@gmail.com>
2023-03-23 13:58:18 +0500

    Add new and update api controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,32 +1,40 @@
 class Api::V1::UsersController < ApplicationController
-  # GET /users/1
-  def show
-    if User.exists?(username: params[:id])
-      @user = User.find_by(username: params[:id])
-      render json: @user, status: :ok
-    else
-      render json: { message: 'Please enter correct username' }, status: :unprocessable_entity
-    end
-  end
+  before_action :authorized, only: [:destroy]
 
-  # POST /users
+  # POST /signup
   def create
-    @user = User.new(user_params)
-    existing_user = User.find_by(username: @user.username)
+    user = User.new(user_params)
 
-    if existing_user
-      render json: { message: 'User already exists' }, status: :unprocessable_entity
-    elsif @user.save
-      render json: @user, status: :created
+    if user.save
+      token = encode_token({ user_id: user.id })
+      render json: { user: { username: user.username, email: user.email, id: user.id }, token: "Bearer #{token}" },
+             status: 201
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: user.errors.full_messages }, status: 400
     end
   end
+
+  # POST /login
+  def login
+    if params[:user].present?
+      user = User.find_by(email: params[:user][:email])
+  
+      if user&.authenticate(params[:user][:password])
+        token = encode_token({ user_id: user.id })
+        render json: { user: { username: user.username, email: user.email, id: user.id }, token: "Bearer #{token}" },
+               status: 201
+      else
+        render json: { error: 'Invalid Email/Password' }, status: 401
+      end
+    else
+      render json: { error: 'Missing user parameters' }, status: 400
+    end
+  end  
 
   private
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
